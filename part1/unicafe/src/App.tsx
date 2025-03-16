1de925c16d8680a8726c4ee4fdd4f2a7c482b7ad
Jake Hong <jakehong0521@berkeley.edu>
2025-03-16 14:32:01 -0700

    Add E1.7 unicafe step 2

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -5,8 +5,25 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad) / all}</div>
+      <div>positive {(good / all) * 100}%</div>
+    </div>
+  );
 };
 
 export default App;
