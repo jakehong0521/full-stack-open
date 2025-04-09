ab4deed29fcefa05f0c83b12796a1c5033969097
Jake Hong <jakehong0521@berkeley.edu>
2025-04-08 20:42:05 -0700

    Add E3.8: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,9 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("reqBody", (req, res) => JSON.stringify(req.body));
+app.use(morgan(":method :url :status - :response-time ms :reqBody"));
 
 let persons = [
   {
