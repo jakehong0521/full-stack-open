e49cb13f85a9d684b4fa47afb24e4ecd31fd32db
Jake Hong <jakehong0521@berkeley.edu>
2025-04-08 19:48:56 -0700

    Add E3.3: Phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,17 @@ app.get("/api/persons", (req, res) => {
   return res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    return res.json(person);
+  } else {
+    return res.status(404).end();
+  }
+});
+
 app.get("/info", (req, res) => {
   const date = new Date();
   const personCount = persons.length;
