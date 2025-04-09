e12b20b0edb8876d8563e6f8af91869e686dce8b
Jake Hong <jakehong0521@berkeley.edu>
2025-04-08 20:21:28 -0700

    Add E3.7: Phonebook backend step 7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const { generateUuid } = require("./utils");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
