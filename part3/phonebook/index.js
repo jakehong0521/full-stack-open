cf1a4ff83b3ea55f257f31dc9cd5c955554896b9
Jake Hong <jakehong0521@berkeley.edu>
2025-04-08 22:05:20 -0700

    Add E3.10 Phonebook backend step 10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -95,6 +95,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
