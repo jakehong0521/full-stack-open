c77d702a84155d91b1fa9258d3bf8fa0a64f9343
Jake Hong <jakehong0521@berkeley.edu>
2025-04-08 22:05:30 -0700

    Add E3.11 Full Stack Phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("reqBody", (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status - :response-time ms :reqBody"));
