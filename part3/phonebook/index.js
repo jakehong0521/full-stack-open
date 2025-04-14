7f3292daa9aaad6289b3048321feec03caefd069
Jake Hong <jakehong0521@berkeley.edu>
2025-04-13 23:36:25 -0700

    Add E3.16: Phonebook database, step 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -91,7 +91,17 @@ app.get("/info", (req, res) => {
   );
 });
 
+const errorHandler = (error, req, res, next) => {
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  res.status(400).send({ error: "something went wrong" });
+};
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(errorHandler);
