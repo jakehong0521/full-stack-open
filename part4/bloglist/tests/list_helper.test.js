d75b2e124235064f857c085c076880f8c4ca43cd
Jake Hong <jakehong0521@berkeley.edu>
2025-09-04 01:23:11 -0700

    Add E4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,6 @@
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
+
 const listHelper = require("../utils/list_helper");
 
 test("dummy returns one", () => {
@@ -7,4 +8,53 @@ test("dummy returns one", () => {
 
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("totalLikes", () => {
+  test("of empty list is zero", () => {
+    const blogs = [];
+
+    assert.strictEqual(listHelper.totalLikes(blogs), 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const numLikes = 521;
+    const blogs = [
+      {
+        __v: 0,
+        _id: "id",
+        author: "author",
+        likes: numLikes,
+        title: "title",
+        url: "https://jakehong0521.github.io",
+      },
+    ];
+
+    assert.strictEqual(listHelper.totalLikes(blogs), numLikes);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const numLikes0 = 521;
+    const numLikes1 = 521;
+    const blogs = [
+      {
+        __v: 0,
+        _id: "id0",
+        author: "author",
+        likes: numLikes0,
+        title: "title",
+        url: "https://jakehong0521.github.io",
+      },
+      {
+        __v: 0,
+        _id: "id1",
+        author: "author",
+        likes: numLikes1,
+        title: "title",
+        url: "https://jakehong0521.github.io",
+      },
+    ];
+
+    assert.strictEqual(listHelper.totalLikes(blogs), numLikes0 + numLikes1);
+  });
 });
