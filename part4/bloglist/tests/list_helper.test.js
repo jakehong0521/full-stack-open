323794d466dda61232b174cfaef9cba1567a1ed2
Jake Hong <jakehong0521@berkeley.edu>
2025-09-04 01:59:15 -0700

    Add E4.6: Helper Functions and Unit Tests, step 4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -54,6 +54,66 @@ describe("favoriteBlog", () => {
   });
 });
 
+describe("mostBlogs", () => {
+  test("of empty list is undefined", () => {
+    const blogs = [];
+
+    assert.strictEqual(listHelper.mostBlogs(blogs), undefined);
+  });
+
+  test("when list has only one blog equals the author of that", () => {
+    const blogs = [
+      {
+        __v: 0,
+        _id: "id",
+        author: "author",
+        likes: 521,
+        title: "title",
+        url: "https://jakehong0521.github.io",
+      },
+    ];
+
+    assert.deepStrictEqual(listHelper.mostBlogs(blogs), {
+      author: "author",
+      blogs: 1,
+    });
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0,
+      },
+    ];
+
+    assert.deepStrictEqual(listHelper.mostBlogs(blogs), {
+      author: "Edsger W. Dijkstra",
+      blogs: 2,
+    });
+  });
+});
+
 describe("totalLikes", () => {
   test("of empty list is zero", () => {
     const blogs = [];
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -14,6 +16,26 @@ const favoriteBlog = (blogs) => {
   return favorite;
 };
 
+const mostBlogs = (blogs) => {
+  let author;
+  let count = 0;
+
+  blogs.reduce((countByAuthor, blog) => {
+    countByAuthor[blog.author] = (countByAuthor[blog.author] || 0) + 1;
+    if (countByAuthor[blog.author] > count) {
+      author = blog.author;
+      count = countByAuthor[blog.author];
+    }
+    return countByAuthor;
+  }, {});
+
+  if (author) {
+    return { author, blogs: count };
+  }
+
+  return undefined;
+};
+
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
@@ -21,5 +43,6 @@ const totalLikes = (blogs) => {
 module.exports = {
   dummy,
   favoriteBlog,
+  mostBlogs,
   totalLikes,
 };
