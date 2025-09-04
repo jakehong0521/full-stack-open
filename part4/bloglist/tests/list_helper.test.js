6723e0b911fc9f58eee3254c95a5a9f39ee19b1e
Jake Hong <jakehong0521@berkeley.edu>
2025-09-04 01:38:34 -0700

    Add E4.5: Helper Functions and Unit Tests, step 3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -10,6 +10,50 @@ test("dummy returns one", () => {
   assert.strictEqual(result, 1);
 });
 
+describe("favoriteBlog", () => {
+  test("of empty list is undefined", () => {
+    const blogs = [];
+
+    assert.strictEqual(listHelper.favoriteBlog(blogs), undefined);
+  });
+
+  test("when list has only one blog equals that blog", () => {
+    const blog = {
+      __v: 0,
+      _id: "id",
+      author: "author",
+      likes: 521,
+      title: "title",
+      url: "https://jakehong0521.github.io",
+    };
+
+    assert.deepStrictEqual(listHelper.favoriteBlog([blog]), blog);
+  });
+
+  test("of a bigger list is returned right", () => {
+    const blogs = [
+      {
+        __v: 0,
+        _id: "5a422a851b54a676234d17f7",
+        author: "Michael Chan",
+        likes: 7,
+        title: "React patterns",
+        url: "https://reactpatterns.com/",
+      },
+      {
+        __v: 0,
+        _id: "5a422aa71b54a676234d17f8",
+        author: "Edsger W. Dijkstra",
+        likes: 5,
+        title: "Go To Statement Considered Harmful",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      },
+    ];
+
+    assert.deepStrictEqual(listHelper.favoriteBlog(blogs), blogs[0]);
+  });
+});
+
 describe("totalLikes", () => {
   test("of empty list is zero", () => {
     const blogs = [];
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,11 +2,24 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const favoriteBlog = (blogs) => {
+  let favorite;
+
+  for (const blog of blogs) {
+    if (!favorite || blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  }
+
+  return favorite;
+};
+
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
