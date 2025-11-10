4f927c02fd07b7c339b7cadca318ffe93deeece0
Jake Hong <jakehong0521@berkeley.edu>
2025-11-09 23:20:55 -0800

    Add E5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}{" "}
+        <button onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? "hide" : "view"}
+        </button>
+      </div>
+
+      {isExpanded && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+export default Blog;
