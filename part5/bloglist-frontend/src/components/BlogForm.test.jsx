3ecfb74cf0b2efbf48da1fc009b4600f23eb1ccf
Jake Hong <jakehong0521@berkeley.edu>
2025-12-03 14:35:18 -0800

    Add E5.16: Blog List Tests, step 4

---
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import BlogForm from './BlogForm'
+
+test('calls createForm when a new blog is created', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+  }
+
+  const createBlogMock = vi.fn()
+  render(<BlogForm createBlog={createBlogMock} />)
+
+  await userEvent.type(screen.getByLabelText('author:'), newBlog.author)
+  await userEvent.type(screen.getByLabelText('title:'), newBlog.title)
+  await userEvent.type(screen.getByLabelText('url:'), newBlog.url)
+  await userEvent.click(screen.getByRole('button', { name: 'create' }))
+
+  expect(createBlogMock).toHaveBeenCalledWith(newBlog)
+})
