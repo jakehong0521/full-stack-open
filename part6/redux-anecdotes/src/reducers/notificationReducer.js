b5fac28b70e122d8e98a7b5c5fa301c2079be9ef
Jake Hong <jakehong0521@berkeley.edu>
2025-12-10 12:26:45 -0800

    Add E6.12 Anecdotes, step 10

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "Initial mock notification";
+
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState,
+  reducers: {
+    addNotification(state, action) {
+      return action.payload;
+    },
+    removeNotification() {
+      return "";
+    },
+  },
+});
+
+export const { addNotification, removeNotification } =
+  notificationSlice.actions;
+export default notificationSlice.reducer;
