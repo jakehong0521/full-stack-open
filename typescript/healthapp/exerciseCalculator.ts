514e1e7cc08d721fdd74c43b7d01de21f227b783
Jake Hong <jakehong0521@berkeley.edu>
2026-08-20 13:55:09 -0700

    Implement [Full Stack Open: TypeScript] Ch.3 7

--- typescript/healthapp/exerciseCalculator.ts
+++ typescript/healthapp/exerciseCalculator.ts
@@ -61,13 +61,15 @@ const parseArguments = (args: string[]): ExerciseInput => {
   }
 };
 
-try {
-  const { hours, target } = parseArguments(process.argv);
-  console.log(calculateExercises(hours, target));
-} catch (error: unknown) {
-  let errorMessage = "Something bad happened.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
+if (process.argv[1] === import.meta.filename) {
+  try {
+    const { hours, target } = parseArguments(process.argv);
+    console.log(calculateExercises(hours, target));
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- typescript/healthapp/index.ts
+++ typescript/healthapp/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { calculateBmi } from "./bmiCalculator.ts";
+import { calculateExercises } from "./exerciseCalculator.ts";
 import { isNotNumber } from "./utils.ts";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
@@ -20,6 +22,27 @@ app.get("/bmi", (req, res) => {
       height: Number(height),
       bmi,
     });
+  }
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || !target) {
+    res.status(400).json({ error: "parameters missing" });
+  } else if (
+    isNotNumber(Number(target)) ||
+    !Array.isArray(daily_exercises) ||
+    daily_exercises.some(isNotNumber)
+  ) {
+    res.status(400).json({ error: "malformatted parameters" });
+  } else {
+    const result = calculateExercises(
+      daily_exercises as number[],
+      target as number,
+    );
+    res.json(result);
   }
 });
 
